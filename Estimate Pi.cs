fc3efb3ca6a6d9fb616e346407149f12796e1320
JulienFink <64968597+JulienFink@users.noreply.github.com>
2020-10-22 13:02:51 +0200

    Add files via upload

---
+++ Estimate Pi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Estimate_Pi
+{
+    class Program
+    {
+        static float estimation_pi(int number_of_points)
+        {
+            int total_points_circle = 0;
+            int total_points_square = 0;
+
+            for(int i = 0; i < number_of_points; i++)
+            {
+                Random generator = new Random();
+                double x = generator.NextDouble();
+                double y = generator.NextDouble();
+
+                double length_squared = x * x + y * y;
+
+                if(length_squared < 1)
+                {
+                    total_points_circle += 1;
+                }
+
+                total_points_square += 1;
+            }
+
+            return (float) 4 * total_points_circle / total_points_square;
+        }
+
+        static void Main(string[] args)
+        {
+            int number_of_points = 100000;
+            float output = estimation_pi(number_of_points);
+            Console.Write($"An estimation of pi could be : {output} .");
+            Console.ReadLine();
+        }
+    }
+}
